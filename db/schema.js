cb041cfc5bdb58bdc686403e787a372cb920a383
ern19 <aarontrammell19@gmail.com>
2017-10-13 14:55:21 -0400

    made schema

---
+++ db/schema.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+
+
+const CommentSchema = new Schema({
+    submittedBy: {
+        type: String,
+        required: true
+    },
+    textContent: {
+        type: String,
+        require: true
+    }    
+})
+
+const CocktailSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    submittedBy: {
+        type: String,
+        required: true
+    },
+    img: {
+        type: String,
+        required: true
+    },
+    recipe: {
+        type: String,
+        required: true
+    },
+    comments: {
+        type: [CommentSchema],
+        required: true
+    }
+})
+
+const UserSchema = {
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    submitted: [CocktailSchema]
+}
+
+const Comment = mongoose.model("Comment", CommentSchema)
+const Cocktail = mongoose.model("Cocktail", CocktailSchema)
+const User = mongoose.model("User", UserSchema)
+
+module.exports = {
+    Comment, Cocktail, User
+}
