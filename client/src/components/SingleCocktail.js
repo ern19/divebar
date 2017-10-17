39a50c2ab45bd6cbed2feef494af5a8f7c1a11a2
ern19 <aarontrammell19@gmail.com>
2017-10-17 14:46:21 -0400

    rendered SingleCocktail component as material ui card

--- client/src/components/SingleCocktail.js
+++ client/src/components/SingleCocktail.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from "axios"
 import RaisedButton from "material-ui/RaisedButton"
-
+import {Card, CardActions, CardHeader, CardMedia, CardText, CardTitle} from "material-ui/Card"
 class SingleCocktail extends Component {
     state={
         user: {}
@@ -22,14 +22,29 @@ class SingleCocktail extends Component {
         console.log("delete clicked")
     }
     render() {
+        const styles = {
+        card: {
+            margin: "auto",
+            width: 650,
+                            
+            overflowY: 'auto',
+        },
+    }
         return (
-            <div>
-                <p>Submitted By: {this.state.user.submittedBy}</p>
-                <p>Cocktail Name: {this.state.user.name}</p>
-                <p>Ingredients: {this.state.user.recipe}</p>
-                <a href={this.state.user.recipeLink}>See the recipe</a>
-                <br/>
-                <RaisedButton onClick={this.deleteCocktail}>Dump it down the drain...</RaisedButton>
+            <div style={styles.card}>
+                
+                <Card>
+                    
+                    <CardMedia
+                    overlay={<CardTitle title={this.state.user.name} subtitle={this.state.user.recipe} />}
+                    >
+                    <img src={this.state.user.img} alt="" />
+                    </CardMedia>
+                    <CardActions>
+                        <RaisedButton onClick={this.deleteCocktail}>Dump it down the drain...</RaisedButton> />
+                        <RaisedButton link={this.state.user.recipeLink}>View the recipe</RaisedButton>
+                    </CardActions>
+                </Card>
             </div>
         );
     }
